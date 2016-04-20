d8cc7f2de4849f9b969b3ef242d946941888b2ac
Kev' Pacheco <kev.pachec.lis@gmail.com>
2016-04-19 20:59:04 -0500

    Agregados colores, un nuevo tema y la actividad del registro, falta terminarlo

--- app/src/main/java/com/example/user/doctoralia/MainActivity.java
+++ app/src/main/java/com/example/user/doctoralia/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.doctoralia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,11 +20,16 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.email)
     EditText edit_email;
 
+    @Bind(R.id.RegisterBtn)
+    Button registerBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        registerListeners();
     }
 
     @OnClick(R.id.login)
@@ -29,5 +37,14 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Iniciando Sesión...", Toast.LENGTH_SHORT).show();
     }
 
+    public void registerListeners(){
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
 }
